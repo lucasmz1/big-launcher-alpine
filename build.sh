53b972f546556a84050d5958bf7118aad0e005aa
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-12-29 20:24:40 -0300

    Fix path for copying big-launcher executables

--- build.sh
+++ build.sh
@@ -14,5 +14,6 @@ echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> ./alp/root/etc/apk/r
 sudo chroot ./alp/root/ /bin/sh -c "apk update && apk upgrade && apk add pkgconfig git build-base sdl3-dev sdl3_ttf-dev sdl3_image-dev make cmake libarchive harfbuzz fmt fmt-dev libxml2 libxml2-dev inih spdlog-dev spdlog && git clone https://github.com/complexlogic/big-launcher.git && cd big-launcher && mkdir build && cd build && cmake .. && make LD_FLAGS='static' && exit"
 #cp ${GITHUB_WORKSPACE}/icon.png ./alp/ && cp ${GITHUB_WORKSPACE}/Big-Launcher.desktop ./alp/ && cp ${GITHUB_WORKSPACE}/AppRun ./alp/
 #ARCH=x86_64 VERSION=clean ./appimagetool -n ./alp/
+cd ${GITHUB_WORKSPACE}
 mkdir dist
-sudo find ./alp/root -type f -executable -iname "*big-launcher*" | xargs -i -t -exec sudo cp {} ./dist/
+sudo find ./alp/root/big-launcher/ -type f -executable -iname "*big-launcher*" | xargs -i -t -exec cp {} ./dist/
