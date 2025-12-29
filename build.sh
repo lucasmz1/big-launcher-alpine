de9dff08a38febb3c69bf404c0a923af0856e491
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2025-12-29 19:11:57 -0300

    Enhance build.sh for improved package installation
    Updated build script to include installation of git and build-base, and streamlined commands within chroot.

--- build.sh
+++ build.sh
@@ -8,10 +8,12 @@ tar xf alpine.tar.gz -C ./alp/root/
 cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/alp/root/etc/
 cd ${GITHUB_WORKSPACE}
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> ./alp/root/etc/apk/repositories
-sudo chroot ./alp/root/ /bin/ash -l -c "apk update && apk upgrade && apk add sdl3 sdl3_ttf sdl3_image make cmake libarchive harfbuzz spdlog fmt libxml2 inih"
-sudo git clone https://github.com/complexlogic/big-launcher.git
-cd big-launcher
-sudo mkdir build && cd build
-sudo cmake -B ..
-sudo make
+sudo chroot ./alp/root/ /bin/ash -l -c "apk update && apk upgrade && apk add git build-base sdl3 sdl3_ttf sdl3_image make cmake libarchive harfbuzz spdlog fmt libxml2 inih \
+git clone https://github.com/complexlogic/big-launcher.git && cd big-launcher \
+mkdir build && cd build \
+cmake .. \
+make \
+mkdir /app \
+make DESTDIR=/app install \
+exit"
 ARCH=x86_64 VERSION=clean ./appimagetool -n ./alp/
