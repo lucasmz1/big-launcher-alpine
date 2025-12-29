a1dfdcf84b9aa2ba4d1b47b61c7554025f004972
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2025-12-29 22:02:58 +0300

    correct path for big-launcher

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ cp /etc/resolv.conf -t ${GITHUB_WORKSPACE}/alp/root/etc/
 cd ${GITHUB_WORKSPACE}
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> ./alp/root/etc/apk/repositories
 sudo chroot ./alp/root/ /bin/ash -l -c "apk update && apk upgrade && apk add sdl3 sdl3_ttf sdl3_image make cmake libarchive harfbuzz spdlog fmt libxml2 inih"
-sudo git clone https://github.com/complexlogic/big-launcher.git /alp/root/big-launcher
-cd /alp/root/big-launcher
-sudo cmake -B ..
+sudo git clone https://github.com/complexlogic/big-launcher.git
+cd big-launcher
+sudo cmake -B
 sudo make
 ARCH=x86_64 VERSION=clean ./appimagetool -n ./alp/
